2359fc87d415a430067e4d541c1943257c4ee938
cristiantudor1607 <andreitudor1607@gmail.com>
2023-12-04 22:36:35 +0200

    idk why but vscode wants cfs uppercase instead of lowercase

--- scheduler/src/schedulers/mod.rs
+++ scheduler/src/schedulers/mod.rs
@@ -13,8 +13,8 @@
 mod priority_queue_pcb;
 pub use priority_queue_pcb::PriorityQueuePCB;
 
-//mod cfs_pcb;
-//pub use cfs_pcb::FairPCB;
+mod cfs_pcb;
+pub use cfs_pcb::FairPCB;
 
 mod round_robin;
 pub use round_robin::RoundRobinScheduler;
@@ -22,11 +22,11 @@ pub use round_robin::RoundRobinScheduler;
 mod priority_queue;
 pub use priority_queue::PriorityQueueScheduler;
 
-//mod cfs;
-//pub use cfs::FairScheduler;
-mod CFS_pcb;
-pub use CFS_pcb::FairPCB;
+mod cfs;
+pub use cfs::FairScheduler;
+// mod CFS_pcb;
+// pub use CFS_pcb::FairPCB;
 
-mod CFS;
-pub use CFS::FairScheduler;
+// mod CFS;
+// pub use CFS::FairScheduler;
 // TODO import your schedulers here
