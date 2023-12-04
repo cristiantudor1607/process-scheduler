9b7d913730c137bf7224ccc66dbab3b176896143
cristiantudor1607 <andreitudor1607@gmail.com>
2023-12-05 00:57:22 +0200

    it's vscode fault, again

--- scheduler/src/schedulers/mod.rs
+++ scheduler/src/schedulers/mod.rs
@@ -13,10 +13,10 @@
 mod priority_queue_pcb;
 pub use priority_queue_pcb::PriorityQueuePCB;
 
-// mod cfs_pcb;
-// pub use cfs_pcb::FairPCB;
-// mod cfs;
-// pub use cfs::FairScheduler;
+mod cfs_pcb;
+pub use cfs_pcb::FairPCB;
+mod cfs;
+pub use cfs::FairScheduler;
 
 mod round_robin;
 pub use round_robin::RoundRobinScheduler;
@@ -24,9 +24,9 @@ pub use round_robin::RoundRobinScheduler;
 mod priority_queue;
 pub use priority_queue::PriorityQueueScheduler;
 
-mod CFS_pcb;
-pub use CFS_pcb::FairPCB;
+// mod CFS_pcb;
+// pub use CFS_pcb::FairPCB;
 
-mod CFS;
-pub use CFS::FairScheduler;
+// mod CFS;
+// pub use CFS::FairScheduler;
 // TODO import your schedulers here
